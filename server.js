d8c075b745984c92ce26e45b95cf9fbd5563e351
Sara Abdo <sarabdo71@gmail.com>
2020-06-17 17:25:09 +0200

    edit issue

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-var http = require("https");
-// Setup empty JS object to act as endpoint for all routes
 projectData = {};
-let apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-let apiCon = ",us";
-let apiKey = "&appid=4cb6d458c8e69784bf3b8589209a199f";
-
-// let apiFurl = apiURL + apiZip + zipId.value + apiCon + apiKey;
 
 // Require Express to run server and routes
 const express = require("express");
@@ -16,83 +9,30 @@ const app = express();
 const bodyParser = require("body-parser");
 // Cors for cross origin allowance
 const cors = require("cors");
-const { json } = require("body-parser");
 
 /* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Initialize the main project folder
+app.use(express.static("website"));
+
 app.listen(8000, function () {
   console.log("Listening at 8000");
 });
 
 app.post("/add", function (req, response) {
-  console.log("Request");
-  console.log("============");
-  console.log(req.body);
-  console.log("============");
-
-  var url = apiURL + req.body.zip + apiCon + apiKey;
-  console.log(url);
-  let reqs = http.get(url, function (res) {
-    let wdata = [];
-    res
-      .on("data", function (dd) {
-        wdata.push(dd);
-      })
-      .on("end", function () {
-        console.log("END");
-        let body = Buffer.concat(wdata);
-        let temp_data = JSON.parse(body.toString());
-        console.log("-------------");
-        console.log(temp_data);
-        if(temp_data.cod=='404'){
-        projectData = {
-          date: req.body.newDate,
-          temp: "Not Found",
-          zip: req.body.zip,
-          con: req.body.feel
-        };
-        
-       }else
-       {
-        projectData = {
-          date: req.body.newDate,
-          temp: temp_data.main.temp,
-          zip: req.body.zip,
-          con: req.body.feel
-        };
-       }
-        console.log("PROEJCT DATA");
-        console.log(projectData);
-        response.send(projectData);
-      });
-  });
-
+  projectData = {
+    date: req.body.date,
+    temp: req.body.main.temp,
+    zip: req.body.zip,
+    content: req.body.content,
+  };
+  console.log(projectData);
+  return response.send({ type: "success" });
 });
 
 app.get("/getData", function (req, res) {
-
-  JSON.stringify(projectData);
-  console.log("****************************");
-  console.log(projectData);
   res.send(projectData);
 });
-
-// Initialize the main project folder
-
-app.use(express.static("website"));
-
-app.use("/api", (request, response) => {
-  console.log("I've got a request");
-  console.log(request.body);
-  const json = request.body;
-  response.json({
-    status: "success",
-    latitude: json.zipId,
-    longitude: json.feel,
-  });
-});
-
-// Setup Server
--- website/app.js
+++ website/app.js
@@ -1,103 +1,65 @@
 /* Global Variables */
 
-let apiURL = "https://api.openweathermap.org/data/2.5/weather?";
-let apiZip = "zip=";
-let zipId = document.getElementById("zip");
-let apiCon = ",us";
-let apiKey = "&appid=4cb6d458c8e69784bf3b8589209a199f";
-let dateDiv = document.getElementById("date");
-let tempDiv = document.getElementById("temp");
-let conDiv  = document.getElementById("content");
-let apiFurl = apiURL + apiZip + zipId.value + apiCon + apiKey;
-let feel = document.getElementById("feelings");
+const APP_URL = "http://localhost:8000";
 
-let APP_URL = "http://localhost:8000";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?";
+const apiZip = "zip=";
+const zipId = document.getElementById("zip");
+const apiCon = ",us";
+const apiKey = "4cb6d458c8e69784bf3b8589209a199f";
+const apiUnit = "imperial";
+
+const dateDiv = document.getElementById("date");
+const tempDiv = document.getElementById("temp");
+const conDiv = document.getElementById("content");
+const feel = document.getElementById("feelings");
+const genId = document.getElementById("generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
-console.log(newDate);
 
-// Personal API Key for OpenWeatherMap API
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
-/*document.getElementById("generate").addEventListener("click", performAction);
-
-function performAction(e) {
-  const getWeather = document.getElementById("feelings").value;
-  getFeel(apiURLkey, getWeather);
+function getData() {
+  fetch(APP_URL + "/getData")
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      if (data.date) {
+        dateDiv.innerHTML = data.date;
+        tempDiv.innerHTML = data.temp;
+        conDiv.innerHTML = data.content;
+      }
+    });
 }
-const getFeel = async (apiURLkey, getWeather) => {
-  //const res = await fetch(apiURLkey );
-};
-console.log(apiURLkey);*/
-
-/* Function to GET Web API Data*/
-
-const getData = async (url = "") => {
-  const request = await fetch(url);
-  try {
-    const allData = await request.json();
-    console.log(getData.JSON);
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-
-const http = apiFurl;
-const option = {
-  method: "GET",
-  header: {
-    apiFurl,
-    "cache-controle": "no-cache",
-  },
-};
-
-/*const req = http.request(option, function(res) {
-  let wdata = [];
-  res.on("data", function (wdata) {
-    wdata.push(wdata);
-  });
-  res.on("end", function () {
-    let body = Buffer.concat(wdata);
-    console.log(body.toString());
-  });
-});
-req.end();
-*/
 
 /* Function to POST data */
-const genId = document.getElementById("generate");
 genId.onclick = function () {
-  zipId;
-  feel;
-  console.log(zipId.value + "__" + feel.value);
-
-  console.log(apiURL + apiZip + zipId.value + apiCon + apiKey);
-
-  let data = { zip: zipId.value, feel: feel.value,newDate:newDate };
-  console.log(APP_URL);
-  console.log(data);
-  let json = sendRequest(APP_URL + "/add", "POST", data).then((data) => {
-  // JSON data parsed by `response.json()` call
-   console.log(data);
-   dateDiv.innerHTML=data.date;
-   tempDiv.innerHTML = data.temp;
-   conDiv.innerHTML=data.con;
-    
-  });
- 
-
-  console.log("JSON")
-  console.log(json);
+  const reqData = { zip: zipId.value + ",us", appid: apiKey, units: apiUnit };
+  var params = new URLSearchParams(reqData).toString();
+  fetch(apiURL + params)
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+      if (data.cod !== "404") {
+        data.zip = zipId.value;
+        data.content = feel.value;
+        data.date = newDate;
+        sendRequest(APP_URL + "/add", data, "POST").then((data) => {
+          getData();
+        });
+      }
+    })
+    .catch(function () {
+      // catch any errors
+    });
 };
 
-async function sendRequest(apiFurl = "", method = "GET", data = {}) {
+async function sendRequest(apiFurl, data = {}, method = "GET") {
   // Default options are marked with *
-console.log(apiFurl);
-  const response = await fetch(apiFurl, {
+  var options = {
     method: method, // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -108,23 +70,13 @@ console.log(apiFurl);
     },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  console.log("Response");
-  console.log(response.body);
+  };
+  if (method != "GET") {
+    options.body = JSON.stringify(data); // body data type must match "Content-Type" header
+  }
+  const response = await fetch(apiFurl, options);
 
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-/* Function to GET Project Data */
-
-/*
-let fjson= sendRequest(APP_URL+"/getData","GET",data).then((data) => { 
-dat= newDate;
-temp = json.stringify(main.temp);
-con = feel.value;
-console.log(data);
-console.log(fjson);
-
-});
-*/
+getData();
